4eb12b20aae0b1bcc109b1bb5ae379c587085a3e
Soumyabrata Sarkar <sarkar.soumyabrata2@gmail.com>
2026-07-25 19:58:50 +0530

    After rebase : Add new test case print statement in Coder.java

--- src/main/java/Coder.java
+++ src/main/java/Coder.java
@@ -3,6 +3,6 @@ public class Coder {
     public static void main(String[] args) {
         System.out.println("Coder");
         System.out.println("New Coder");
-        // some test cases
+        System.out.println("Added new test cases");
     }
 }
