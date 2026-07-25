7028f56a5a17fb0cd1ee0475f90059a1f89b6f0f
Soumyabrata Sarkar <sarkar.soumyabrata2@gmail.com>
2026-07-25 19:58:50 +0530

    Add new test case print statement in Coder.java

--- src/main/java/Coder.java
+++ src/main/java/Coder.java
@@ -3,5 +3,6 @@ public class Coder {
     public static void main(String[] args) {
         System.out.println("Coder");
         System.out.println("New Coder");
+        System.out.println("Added new test cases");
     }
 }
